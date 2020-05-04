0d1cce249c610c6df63ff0368844a2ce90595bab
OwenTam <64348176+owentamhw@users.noreply.github.com>
2020-05-04 18:01:35 +0800

    added drawdown, more portfolio stat

--- rebalance_backtest.py
+++ rebalance_backtest.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 from efficient_frontier_strat import efficient_frontier_strat
+from portfolio_statistic import *
 
 
 ##############################
@@ -16,8 +17,8 @@ from efficient_frontier_strat import efficient_frontier_strat
 # getdata(test, start, end, True)
 
 # read prices data
-path='./stock_data/test_data.csv'
-#path='./stock_data/20-05-03_165042.csv'
+#path='./stock_data/test_data.csv'
+path='./stock_data/20-05-03_165042.csv'
 
 prices = pd.read_csv(path)
 
@@ -55,61 +56,63 @@ if is_save_weight_allocations_to_csv:
   path = './weight_allocations/df_min_vol_' + datetime.now().strftime('%y-%m-%d_%H%M%S') + '.csv'
   df_min_vol.to_csv(path, index = False, header=True)
 
-## This function evaluate the cumulative returns, portfolio annualized returns and volatilities,
-## sharpe ratio.
+# Function for evaluating statistic for a portfolio given the list of rebalance day and allocations of assets
 def get_portfolio_stat(prices: pd.DataFrame, weight: list, rebalance_tradeday_pos: list, risk_free_rate: float = 0.0) -> tuple:
-  ret_plus_1 = prices.iloc[:,1:].pct_change() + 1
-  # Set up the initial allocations of assets
-  new_weight = weight[0] * 1
-  cumulative_ret = [pd.DataFrame([new_weight], index=[prices.Date.iloc[rebalance_tradeday_pos[0]]], columns=prices.columns[1:])]
-  for i in range(1, len(rebalance_tradeday_pos)):
-    # Calculate the cumulative returns of assets with new weight 
-    start = rebalance_tradeday_pos[i-1] + 1; end = rebalance_tradeday_pos[i] + 1
-    # Calculate the current portfolio value and determine the new weights of assets
-    cumulative_ret.append((ret_plus_1.iloc[start:end, :].cumprod() * new_weight))
-    new_weight = weight[i] * cumulative_ret[-1].tail(1).sum(axis=1).values
-  cumulative_ret.append((ret_plus_1.iloc[rebalance_tradeday_pos[-1]+1:, :].cumprod() * new_weight))
-  # Merge the list of df into a single one
-  cumulative_ret = pd.concat(cumulative_ret)
-  cumulative_ret_history = cumulative_ret.sum(axis=1)
-  cumulative_ret_history.index = prices.Date[rebalance_tradeday_pos[0]:]
+  # Get the portfolio cumulative return history
+  cumulative_ret_history = get_port_cumulative_return(prices, weight, rebalance_tradeday_pos)
+  
+  # Get the drawdown of portfolio
+  drawdown_history = get_drawdown(cumulative_ret_history)
+  max_daily_drawdown = min(drawdown_history)
+  df = drawdown_history.reset_index(drop=True)
+  max_drawdown_duration = df[df==0.0].reset_index().diff()['index'].max()
+  
   # Portfolio daily returns under this strategy
   port_ret = cumulative_ret_history.pct_change()[1:]
+  
   annualized_port_return = cumulative_ret_history[-1] ** (252/(len(port_ret))) - 1
-  annualized_port_std = np.sqrt(252) * port_ret.std(ddof=0) # ddof=0 for population std
-  sharpe_ratio = annualized_port_return / annualized_port_std
-  port_stat = {'cumulative_ret': cumulative_ret_history[-1] - 1, 
-              'annual_ret': annualized_port_return,
+  annualized_port_std = np.sqrt(252) * port_ret.std() 
+  sharpe_ratio = annualized_port_return - risk_free_rate / annualized_port_std
+  sortino_ratio = get_sortino_ratio(port_ret, 0.0, risk_free_rate)
+  
+  port_stat = {'annual_ret': annualized_port_return,
               'annual_std': annualized_port_std,
-              'sharpe_ratio': sharpe_ratio}
-  return port_stat, cumulative_ret_history
+              'cumulative_ret': cumulative_ret_history[-1] - 1,
+              'max_daily_drawdown': max_daily_drawdown,
+              'max_drawdown_duration': max_drawdown_duration,
+              'sharpe_ratio': sharpe_ratio,
+              'sortino_ratio': sortino_ratio}
+  # rounding values to 4 decimal places
+  for k, v in port_stat.items():
+    port_stat[k] = round(v, 4)
+  return port_stat, cumulative_ret_history, drawdown_history
 
 ### Get the portfolio cumulative returns with different strategies
 # With the optimized 
 sharpe_weight = df_sharpe.w.to_list()
 minvol_weight = df_min_vol.w.to_list()
-max_sharpe_port_stat, max_sharpe_cumulative_ret_history = get_portfolio_stat(
-    prices, sharpe_weight, rebalance_tradeday_monthly_pos, risk_free_rate)
-min_vol_port_stat, min_vol_cumulative_ret_history = get_portfolio_stat(
-    prices, minvol_weight, rebalance_tradeday_monthly_pos, risk_free_rate)
+max_sharpe_port_stat, max_sharpe_cumulative_ret_history, max_sharpe_drawdown_history = \
+    get_portfolio_stat(prices, sharpe_weight, rebalance_tradeday_monthly_pos, risk_free_rate)
+min_vol_port_stat, min_vol_cumulative_ret_history, min_vol_drawdown_history = \
+    get_portfolio_stat(prices, minvol_weight, rebalance_tradeday_monthly_pos, risk_free_rate)
 
 # Naive diversification w/o rebalance (1/N Portfolio) 
 weight = [[1/num_assets for _ in range(num_assets)]]
-naive_port_stat, naive_port_cumulative_ret_history = get_portfolio_stat(
-    prices, weight, [rebalance_tradeday_monthly_pos[0]], risk_free_rate)
+naive_port_stat, naive_port_cumulative_ret_history, naive_port_drawdown_history = \
+    get_portfolio_stat(prices, weight, [rebalance_tradeday_monthly_pos[0]], risk_free_rate)
 
 # Naive diversification w/ rebalance (1/N Portfolio)
 # Rebalance to 1/N portfolio monthly according to adj. close price on last trading day on last month
 # Evaluate the weight allocations every month
 naive_rebalance_weight = [[1/num_assets for _ in range(num_assets)]  for _ in range(len(rebalance_tradeday_monthly_pos))]
-naive_port_rebalance_stat, naive_port_rebalance_cumulative_ret_history = get_portfolio_stat(
-    prices, naive_rebalance_weight, rebalance_tradeday_monthly_pos, risk_free_rate)
+naive_port_rebalance_stat, naive_port_rebalance_cumulative_ret_history, naive_port_rebalance_drawdown_history = \
+    get_portfolio_stat(prices, naive_rebalance_weight, rebalance_tradeday_monthly_pos, risk_free_rate)
 
 # Print the porfolio statistic of different strategies
-print('Max sharpe ratio portfolio stats:\n', max_sharpe_port_stat)
-print('Min volatility portfolio stats:\n', min_vol_port_stat)
-print('Naive diversification w/o rebalance (1/N Portfolio):\n', naive_port_stat)
-print('Naive diversification w/ rebalance (1/N Portfolio):\n', naive_port_rebalance_stat)
+print('Max sharpe ratio portfolio stats:\n', pd.DataFrame(max_sharpe_port_stat, index=['Max Sharpe']), '\n')
+print('Min volatility portfolio stats:\n', pd.DataFrame(min_vol_port_stat, index=['Min Vol']), '\n')
+print('Naive diversification without rebalance (1/N Portfolio):\n', pd.DataFrame(naive_port_stat, index=['1/N w/o rebalance']), '\n')
+print('Naive diversification with rebalance (1/N Portfolio):\n', pd.DataFrame(naive_port_rebalance_stat, index=['1/N w/ rebalance']), '\n')
 
 ###############################################################
 # Drawing
@@ -119,7 +122,7 @@ plt.figure()
 plt.plot(max_sharpe_cumulative_ret_history, label='Max sharpe ratio')
 plt.plot(min_vol_cumulative_ret_history, label='Min volatility')
 plt.plot(naive_port_cumulative_ret_history, label='Naive diversification w/o rebalance (1/N Portfolio)')
-plt.plot(naive_port_rebalance_cumulative_ret_history, label='Naive diversification w rebalance (1/N Portfolio)')
+plt.plot(naive_port_rebalance_cumulative_ret_history, label='Naive diversification with rebalance (1/N Portfolio)')
 plt.legend()
 plt.ylabel('Cumulative returns', fontsize=15)
 plt.title('Cumulative returns with different strategies', fontsize=15)
@@ -149,5 +152,15 @@ plt.title('Assets allocations (Min Volatility)', fontsize=15)
 plt.xticks(ind, rebalance_tradeday_monthly.dt.strftime('%y/%m/%d').to_list(), rotation=90)
 plt.yticks(np.arange(0, 1.1, 0.1))
 plt.legend((b[0] for b in lst_bar), (prices.columns[1:].to_list()))
-plt.show()
 
+# Firgure 4. Drawdown with different strategies
+plt.figure()
+plt.plot(max_sharpe_drawdown_history, label='Max sharpe ratio')
+plt.plot(min_vol_drawdown_history, label='Min volatility')
+plt.plot(naive_port_drawdown_history, label='Naive diversification w/o rebalance (1/N Portfolio)')
+plt.plot(naive_port_rebalance_drawdown_history, label='Naive diversification with rebalance (1/N Portfolio)')
+plt.legend()
+plt.axhline(color='r')
+plt.ylabel('Maximum Draw Down (% change in decimal)', fontsize=15)
+plt.title('Drawdown with different strategies', fontsize=15)
+plt.show()
